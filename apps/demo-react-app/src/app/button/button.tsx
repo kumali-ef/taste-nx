e2a250b083cde3343ff602901dd7619a17532bd6
Kuma Li <kuma.li@ef.com>
2022-06-07 22:49:04 +0800

    add an tailwind styled material button demo

--- apps/demo-react-app/src/app/button/button.tsx
+++ apps/demo-react-app/src/app/button/button.tsx
@@ -1,24 +1,27 @@
 import ButtonUnstyled from '@mui/base/ButtonUnstyled';
-import { styled, Theme } from '@mui/system';
-import tw from 'tailwind.macro';
+import styled from '@emotion/styled';
+import tw from 'twin.macro';
 
 /* eslint-disable-next-line */
 export interface ButtonProps {
   text?: string,
 }
 
-const CustomButton = styled(ButtonUnstyled)(
-  ({ theme }: { theme: Theme }) => `
-    padding: 10px;
-    border-radius: 5px;
-    background-color: purple;
-    color: white;
-    ${tw`bg-black`}
+const CustomButton = styled(ButtonUnstyled)(tw`
+    p-2
+    rounded-md
+    bg-purple-500
+    text-red-500
+    hover:bg-gray-500
 `);
+
+const GrayButton = styled(CustomButton)(tw`
+  bg-gray-500
+`)
 
 export function Button(props: ButtonProps) {
   return (
-    <CustomButton >
+    <CustomButton>
       {props.text || 'Welcome to Custom Button of el-web-app!'}
     </CustomButton>
   );
